9b484dbe81031b523ec2a9a558b23e4b00f724eb
Tim Harbeck <tim.harbeck@harbeckdom.de>
2024-03-15 18:53:53 +0100

    fix: added base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'crispynuggat2.github.io'
+    site: 'crispynuggat2.github.io',
+    base: '/master',
 });
